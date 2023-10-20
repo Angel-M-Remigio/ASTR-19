36c44533b4b522fb89c02494e13a945bceb9dbe2
Angel-M-Remigio <146384984+Angel-M-Remigio@users.noreply.github.com>
2023-10-20 15:21:56 -0700

    Code journal 1 second entry

---
+++ cj1-2.py
@@ -0,0 +1,5 @@
+print("Floating point addition: 11.829182 + 29.39291 =", 11.829182 + 29.39291, "sum data type =", type(11.829182 + 29.39291))
+
+print("Integer Subtraction: 10 - 2 = ", 10-2, "res data type =", type(10 - 2))
+
+print("Floating point and integer product: 32.12334 * 2 =", 32.12334 * 2, "product data type =", type(32.12334 * 2))
